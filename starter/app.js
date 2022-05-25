2f49d78c427af41bf89112521ebdba50d635231c
41dr3w <aavlqz@gmail.com>
2022-05-25 02:06:52 -0300

    "adding progress"

--- starter/app.js
+++ starter/app.js
@@ -1,30 +1,47 @@
-//Contador del jugador uno.
-//Crear un objeto el cual contenta el puntaje del primer jugador
-//Selecionar el boton del jugador uno
-//Selecionar el display del jugador uno
+var p1Display = document.getElementById("p1Display")
+var p2Display = document.getElementById("p2Display")
 
-//Contador del jugador dos.
-//Crear un objeto el cual contenta el puntaje del segundo jugador
-//Seleccionar el boton del jugador dos
-//Selecionar el display del jugador dos
+const playTo = document.get
+const selected = playTo.Index
+var valueP1 = 0, valueP2 = 0
 
-//Selecionar el boton de reset
+const btnOne = document.getElementById("p1Button")
+const btnTwo = document.getElementById("p2Button")
+const resetButton = document.getElementById("reset")
 
-// Selecionar el texto de quien juega
+btnOne.addEventListener("click", function() {
+    p1Display.innerText=valueP1++
+    display()
+}); 
 
-// // Declarar variable de cuantos juegos se haran
+btnTwo.addEventListener("click", function() {
+    p2Display.innerText=valueP2++
+    display()
+}); 
 
-// //Declarar booleano para saber si el juego termino o no
+resetButton.addEventListener("click",function(){reSet()})
 
-//Funcion para actualizar los scores, debe cambiar los colores si el juego termino!
-// 'has-text-success' (Color verde)
-// 'has-text-danger' (Color rojo)
-// Si un score llega al winning score, es game over!
+function display(){
+    if(p1Display===selected&&p1Display>p2Display){
+        p1Display.innerHTML.getAttribute("color")="green"
+        p2Display.innerHTML.getAttribute("color")="red"
+    }
+    else(p2Display===seLected&&p2Display>p1Display)
+        p2Display.innerText=valueP2
+        p1Display.innerHTML.getAttribute("color")="green"
+        p2Display.innerHTML.getAttribute("color")="red"
+    
+    //DESHABILITAR BOTONES.
+}
 
-//Poner la funcion update scores a los botones de ambos jugadores!
+function reSet() {valueP1=0;valueP2=0;
+    p1Display.innerText=valueP1;
+    p2Display.innerText=valueP2;}
 
-//Poner la funcion de reset al boto nde reset
-
-// resetButton.addEventListener("click", reset);
-
-// // Funcion para reiniciar todo!
+function seLector(presel){
+    for(var i=0; i<presel.length; i++){
+        if(presel[i].selectedIndex==true)
+        {  var posel=presel[i].value }
+        }
+        return posel;
+}
